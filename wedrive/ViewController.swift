2be9954f0986d0ec7f4ae77f3a5eb6c5fce69969
Manab Chetia <manab.chetia@outlook.com>
2015-07-18 02:07:23 +1000

    Initial Commit

---
+++ wedrive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wedrive
+//
+//  Created by Manab Chetia on 18/07/2015.
+//  Copyright (c) 2015 Manab Chetia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
