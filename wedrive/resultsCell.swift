216a23ae473cc0b073e9adbc6de552f176fd0cc2
Manab Chetia <manab.chetia@outlook.com>
2015-07-19 01:04:23 +1000

    Things work

---
+++ wedrive/resultsCell.swift
@@ -0,0 +1,24 @@
+//
+//  resultsCell.swift
+//  wedrive
+//
+//  Created by Manab Chetia on 18/07/2015.
+//  Copyright (c) 2015 Manab Chetia. All rights reserved.
+//
+
+import UIKit
+
+class resultsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ wedrive/usersVC.swift
@@ -0,0 +1,13 @@
+//
+//  usersVC.swift
+//  wedrive
+//
+//  Created by Manab Chetia on 18/07/2015.
+//  Copyright (c) 2015 Manab Chetia. All rights reserved.
+//
+
+import Cocoa
+
+class usersVC: UIViewController {
+
+}
