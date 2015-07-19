ddfe5befab4bd19ab8d0f41bc3b4f86d71374691
Manab Chetia <manab.chetia@outlook.com>
2015-07-19 21:59:49 +1000

    Things work

---
+++ wedrive/conversationsVC.swift
@@ -0,0 +1,35 @@
+//
+//  conversationsVC.swift
+//  wedrive
+//
+//  Created by Manab Chetia on 19/07/2015.
+//  Copyright (c) 2015 Manab Chetia. All rights reserved.
+//
+
+import UIKit
+
+class conversationsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
