302cc676029f7fa664668280cb3504aa3ca42b12
Manab Chetia <manab.chetia@outlook.com>
2015-08-04 23:54:13 +1000

    Messages work

---
+++ wedrive/messageCell.swift
@@ -0,0 +1,24 @@
+//
+//  messageCell.swift
+//  wedrive
+//
+//  Created by Manab Chetia on 4/08/2015.
+//  Copyright (c) 2015 Manab Chetia. All rights reserved.
+//
+
+import UIKit
+
+class messageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
